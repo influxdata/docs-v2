d50cf9951e1188ff1e19ba8d1e8d1a7088e25e27
Gunnar Aasen <gunnar.r.aasen@gmail.com>
2019-01-23 11:50:57 -0800

    Use edge lambda function and remove path redirects.

---
+++ deploy/edge.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const path = require('path');
+const originV1 = process.env.ORIGIN_V1;
+
+exports.handler = (event, context, callback) => {
+  const { request } = event.Records[0].cf;
+  const { uri } = request;
+  const parsedPath = path.parse(uri);
+  
+  const indexPath = 'index.html';
+  const validExtension = {
+    'html': true,
+    'css': true,
+    'js': true,
+    'xml': true,
+    'png': true,
+    'svg': true,
+  };
+
+  // Do not alter any requests with a valid file extension.
+  if (!validExtension[parsedPath.ext]) {
+    callback(null, request);
+  }
+
+  // Redirect root path to v2 docs.
+  if (uri == '/') {
+    callback(null, {
+        status: '302',
+        statusDescription: 'Found',
+        headers: {
+          location: [{
+            key: 'Location',
+            value: '/v2.0/',
+          }],
+        }
+      });
+  }
+
+  // Redirect to slash path.
+  if (!uri.endsWith('/')) {
+    callback(null, {
+        status: '302',
+        statusDescription: 'Found',
+        headers: {
+          location: [{
+            key: 'Location',
+            value: uri + '/',
+          }],
+        }
+      });
+  }
+
+  // Return page if it's a valid clean URI that ends with a slash.
+  request.uri = uri + indexPath;
+  callback(null, request);
+};
