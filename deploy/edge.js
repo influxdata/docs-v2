bcb3a7b873a7e7f6ed452b648792ad82bede64c6
Gunnar Aasen <gunnar.r.aasen@gmail.com>
2019-01-23 13:46:48 -0800

    More bug fixes to CF template and function

--- deploy/edge.js
+++ deploy/edge.js
@@ -1,26 +1,46 @@
 'use strict';
 
-exports.handler = function handler(event, context, callback) => {
+exports.handler = (event, context, callback) => {
   const { request } = event.Records[0].cf;
   const { uri, headers, origin } = request;
-  const lastChar = uri.Slice(-1);
   const extension = uri.substr(uri.lastIndexOf('.') + 1);
 
-  const validExtensions = ['html', 'css', 'js', 'xml', 'png', 'svg', 'otf', 'eot', 'ttf', 'woff'];
+  const validExtensions = ['.html', '.css', '.js', '.xml', '.png', '.svg', '.otf', '.eot', '.ttf', '.woff'];
   const indexPath = 'index.html';
 
-  // Append "index.html" to origin request
-  if (lastChar == '/') {
+  // If path ends with '/', then append 'index.html', otherwise redirect to a
+  // path with '/' or ignore if the path ends with a valid file extension.
+  if (uri.endsWith('/')) {
     request.uri = uri + indexPath;
-  } else if (!validExtensions.includes(extension)) {
-    request.uri = uri + '/' + indexPath;
+  } else if (uri.endsWith('/index.html')) {
+    callback(null, {
+        status: '302',
+        statusDescription: 'Found',
+        headers: {
+          location: [{
+            key: 'Location',
+            value: uri.substr(0, uri.length - indexPath.length),
+          }],
+        }
+      });
+  } else if (validExtensions.filter((ext) => uri.endsWith(ext)) == 0) {
+    callback(null, {
+        status: '302',
+        statusDescription: 'Found',
+        headers: {
+          location: [{
+            key: 'Location',
+            value: uri + '/',
+          }],
+        }
+      });
   }
 
   const pathsV1 = ['/influxdb', '/telegraf', '/chronograf', '/kapacitor', '/enterprise_influxdb', '/enterprise_kapacitor'];
   const originV1 = process.env.ORIGIN_V1;
 
   // Send to v1 origin if start of path matches 
-  if (pathsV1.filter((path) => path.startsWith(request.uri)) > 0) {
+  if (pathsV1.filter((path) => uri.startsWith(path)) > 0) {
     headers['host'] = [{key: 'host', value: originV1}];
     origin.s3.domainName = originV1;
   }
