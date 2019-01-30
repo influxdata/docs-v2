875bf8eff555a016d73658dce7d2b17733055411
Scott Anderson <scott@influxdata.com>
2019-01-30 13:51:32 -0800

    updated search styles to remove unnecessarily repetitive elements

--- assets/js/search-interactions.js
+++ assets/js/search-interactions.js
@@ -1,7 +1,9 @@
+// Fade content wrapper when focusing on search input
 $('#algolia-search-input').focus(function() {
   $('.content-wrapper').fadeTo(400, .35);
 })
 
+// Hide search dropdown when leaving search input
 $('#algolia-search-input').blur(function() {
   $('.content-wrapper').fadeTo(200, 1);
   $('.ds-dropdown-menu').hide();
