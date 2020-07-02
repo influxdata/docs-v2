987ce7b9caebaa0349b4854102a3a7c856e87608
Scott Anderson <scott@influxdata.com>
2020-07-02 11:08:01 -0600

    fixed typo in js comment

--- assets/js/influxdb-url.js
+++ assets/js/influxdb-url.js
@@ -161,7 +161,7 @@ setRadioButton(getUrl())
 
 /////////////////////////// Dynamically update URLs ///////////////////////////
 
-// Extract the protocol and hostname of refferrer
+// Extract the protocol and hostname of refferer
 referrerHost = document.referrer.match(/^(?:[^\/]*\/){2}[^\/]+/g)[0]
 
 // Check if the referrerHost is one of the cloud URLs
