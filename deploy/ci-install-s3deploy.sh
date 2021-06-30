6760eaea559facb6f10dfa779ad9c1d4021fe936
pierwill <pierwill@users.noreply.github.com>
2021-06-30 15:37:48 -0500

    fix: define checksum in .circleci/config.yml

--- deploy/ci-install-s3deploy.sh
+++ deploy/ci-install-s3deploy.sh
@@ -1,10 +1,7 @@
 set -x
 set -e
 
-# S3DEPLOY_VERSION is defined in .circleci/config.yml
 S3DEPLOY_DOWNLOAD=s3deploy_${S3DEPLOY_VERSION}_Linux-64bit.tar.gz
-# From https://github.com/bep/s3deploy/releases
-S3DEPLOY_HASH="95de91ed207ba32abd0df71f9681c1ede952f8358f3510b980b02550254c941a"
 
 # Install s3deploy if not already cached or upgrade an old version.
 if [ ! -e $HOME/bin/s3deploy ] || ! [[ `$HOME/bin/s3deploy -V` =~ ${S3DEPLOY_VERSION} ]]; then
