209f563cfbd04908c10e92da7e3ec0eb5488fbb2
Jason Stirnaman <jstirnaman@influxdata.com>
2021-08-23 11:05:07 -0500

    View, create, and use tokens with the API (#3049)
    * fix: broken links in Use Tokens
    
    * feature: add Create and View Tokens with the API
    
    * update: View, create, and use tokens with the API. Add command-line, curl examples. (#1541)
    
    * Update content/influxdb/v2.0/api-guide/api_intro.md
    
    * Update token description
    
    Co-authored-by: pierwill <19642016+pierwill@users.noreply.github.com>

---
+++ shared/text/api/v2.0/auth/oss/tokens-view-filter.sh
@@ -0,0 +1,27 @@
+# The example below uses the common `curl` and `jq` command-line tools
+# with the InfluxDB API to do the following:
+# 1. Find a user by username and extract the user ID.
+# 2. Find the user's authorizations by user ID.
+# 3. Filter for `active` authorizations that have `write` permission.
+
+INFLUX_TOKEN=YOUR_API_TOKEN
+
+function list_write_auths() {
+  curl "http://localhost:8086/api/v2/users/?name=$1" \
+    --header "Authorization: Token ${INFLUX_TOKEN}" \
+    --header 'Content-type: application/json' | \
+  
+  jq --arg USER $1 '.users[] | select(.name == $USER) | .id' | \
+  
+  xargs -I '%' \
+  curl "http://localhost:8086/api/v2/authorizations/?userID=%" \
+    --header "Authorization: Token ${INFLUX_TOKEN}" \
+    --header 'Content-type: application/json' | \
+  
+  jq '.authorizations[]
+        | select(.permissions[] | select(.action=="write"))
+        | select(.status=="active")'
+}
+
+list_write_auths 'iot_user_1'
+
