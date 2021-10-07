e53b32a831b3f3b315330d3eea606b15939f8f63
Jason Stirnaman <stirnamanj@gmail.com>
2021-10-07 12:08:33 -0500

    Revert "Fix api docs dependencies (#3226)"
    This reverts commit a70df65a797e540c1ca2d2730383fd373e819a73.

--- api-docs/generate-api-docs.sh
+++ api-docs/generate-api-docs.sh
@@ -40,10 +40,8 @@ weight: 304
 ---
 "
 
-  redocCLI="redoc-cli@0.12.3"
-
   # Use Redoc to generate the v2 API html
-  npx --yes $redocCLI bundle $version/swagger.yml \
+  npx redoc-cli bundle $version/swagger.yml \
     -t template.hbs \
     --title="InfluxDB $titleVersion API documentation" \
     --options.sortPropsAlphabetically \
@@ -54,7 +52,7 @@ weight: 304
 
 
   # Use Redoc to generate the v1 compatibility API html
-  npx --yes $redocCLI bundle $version/swaggerV1Compat.yml \
+  npx redoc-cli bundle $version/swaggerV1Compat.yml \
     -t template.hbs \
     --title="InfluxDB $titleVersion v1 compatibility API documentation" \
     --options.sortPropsAlphabetically \
