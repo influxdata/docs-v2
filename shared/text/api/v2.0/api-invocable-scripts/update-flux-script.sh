ae5b8d1785795902e08d652fda3de895e72394ae
Jason Stirnaman <jstirnaman@influxdata.com>
2021-10-20 16:35:32 -0500

    feat: add find_and_update example (#2547).

---
+++ shared/text/api/v2.0/api-invocable-scripts/update-flux-script.sh
@@ -0,0 +1,21 @@
+find_and_update() {
+  script=$(curl -X 'GET' \
+    "http://localhost:8086/api/v2/functions" \
+    --header "Authorization: Token ${INFLUX_API_TOKEN}" \
+    --header 'Accept: application/json' \
+    --header 'Content-Type: application/json' \
+    --data-urlencode 'org=jstirnamaninflux&limit=10' \
+    | jq '[.functions[] | select(.script | test("start: -?\\d\\w"))]' \
+    | jq '.[0]')
+  new_script=$(jq '.script |= sub("start: .*d"; "start: params.myrangestart")' <<< "${script}")
+  script_id=$(jq -r '.id' <<< "${new_script}")
+
+  curl -X 'PATCH' \
+    "${INFLUX_URL}/api/v2/functions/${script_id}" \
+    --header "Authorization: Token ${INFLUX_API_TOKEN}" \
+    --header 'Accept: application/json' \
+    --header 'Content-Type: application/json' \
+    --data "${new_script}" | jq .
+}
+
+find_and_update
