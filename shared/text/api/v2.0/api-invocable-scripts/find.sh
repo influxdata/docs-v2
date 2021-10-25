6587ee1fdc7fef7a3119735a837aa0ef04a8a426
Jason Stirnaman <jstirnaman@influxdata.com>
2021-10-25 08:30:31 -0500

    Document new API Invocable Scripts feature (was named-functions) (#3282)
    * feat: add api-guides/guides and guides/api-invocable-scripts for the API-invocable scripts (managed functions) feature (##2547, #2233).
    
    * feat: document API-invocable scripts. Add example for creating and invoking a script with POST. Unable to make GET work with query parameters.
    
    * feat: moved API-invocable scripts under api-guide. Remove guides directory. Add docs for list and find. Add example for list. Add shortcuts and glossary links. Verified with team-compute that GET /invoke and python are not supported yet.
    
    * feat: rename example function for simplicity since there's no GET for now.
    
    * feat: add find_and_update example (#2547).
    
    * feat: add update example. Clarify, expand invoke example. Add links. (#2547)
    
    * Update content/influxdb/cloud/api-guide/api-invocable-scripts/_index.md
    
    Co-authored-by: Scott Anderson <sanderson@users.noreply.github.com>
    
    * fix: remove accidental commit of generated redoc-static.html
    
    * feat: manual update to cloud swagger. Merges meta, auth, tags, and x-tagGroups from src/cloud.yml to aggregate contracts/ref/cloud.yml.
    
    * fix: replace /functions with /scripts
    
    * feat: get swaggers from contracts/ref. Still requires copy-paste of info, tags, and auth from contracts/cloud.yml and contracts/oss.yml
    
    * feat: invocable scripts (#2547)
    
    * Update content/influxdb/cloud/api-guide/api-invocable-scripts/_index.md
    
    Co-authored-by: pierwill <19642016+pierwill@users.noreply.github.com>
    
    * feat: new section for params (#2547).
    
    * fix: swagger server url (#2547)
    
    * Update content/influxdb/cloud/api-guide/api-invocable-scripts/_index.md
    
    Co-authored-by: Scott Anderson <sanderson@users.noreply.github.com>
    
    * feat: remove jq and function where there's no clear need.
    
    Co-authored-by: pierwill <19642016+pierwill@users.noreply.github.com>

---
+++ shared/text/api/v2.0/api-invocable-scripts/find.sh
@@ -0,0 +1,5 @@
+curl -X 'GET' \
+  "${INFLUX_URL}/api/v2/scripts/${SCRIPT_ID}" \
+  --header "Authorization: Token ${INFLUX_API_TOKEN}" \
+  --header 'accept: application/json' \
+  --header 'Content-Type: application/json'
---
+++ shared/text/api/v2.0/api-invocable-scripts/update-flux-script.sh
@@ -0,0 +1,21 @@
+find_and_update() {
+  script=$(curl -X 'GET' \
+    "http://cloud2.influxdata.com/api/v2/scripts" \
+    --header "Authorization: Token ${INFLUX_API_TOKEN}" \
+    --header 'Accept: application/json' \
+    --header 'Content-Type: application/json' \
+    --data-urlencode 'org=jstirnamaninflux&limit=10' \
+    | jq '[.scripts[] | select(.script | test("start: -?\\d\\w"))]' \
+    | jq '.[0]')
+  new_script=$(jq '.script |= sub("start: .*d"; "start: params.myrangestart")' <<< "${script}")
+  script_id=$(jq -r '.id' <<< "${new_script}")
+
+  curl -X 'PATCH' \
+    "${INFLUX_URL}/api/v2/scripts/${script_id}" \
+    --header "Authorization: Token ${INFLUX_API_TOKEN}" \
+    --header 'Accept: application/json' \
+    --header 'Content-Type: application/json' \
+    --data "${new_script}" | jq .
+}
+
+find_and_update
