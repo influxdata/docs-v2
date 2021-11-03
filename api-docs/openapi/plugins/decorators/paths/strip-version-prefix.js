88fb48beab7c4f75f1d14c32fc57a2662a6341e1
Jason Stirnaman <jstirnaman@influxdata.com>
2021-11-03 13:58:17 -0500

    chore: whitespace.

--- api-docs/openapi/plugins/decorators/paths/strip-version-prefix.js
+++ api-docs/openapi/plugins/decorators/paths/strip-version-prefix.js
@@ -5,18 +5,24 @@ function StripVersionPrefix() {
   return {
     PathMap: {
       leave(paths, ctx) {
-	const nonversioned = ['/health', '/legacy/authorizations', '/legacy/authorizations/{authID}', '/legacy/authorizations/{authID}/password', '/ping', '/ready']; 
+        const nonversioned = [
+          '/health',
+          '/legacy/authorizations',
+          '/legacy/authorizations/{authID}',
+          '/legacy/authorizations/{authID}/password',
+          '/ping',
+          '/ready'
+        ];
         const prefix = '/api/v2';
-	nonversioned.forEach(function(nv) {
+        nonversioned.forEach(function(nv) {
           const path = JSON.stringify(paths[prefix + nv]);
-	  if(path) {
+          if(path) {
             delete paths[prefix + nv];
-	    paths[nv] = JSON.parse(path);
-	  }
-	});
+            paths[nv] = JSON.parse(path);
+          }
+        });
       }
     }
   }
 }
-
 
