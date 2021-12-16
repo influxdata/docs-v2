dce5ad83f0206a077facb94b99b0174489cf5282
Jason Stirnaman <jstirnaman@influxdata.com>
2021-12-16 14:30:01 -0600

    fix: add replace-shortcodes decorator. Fix v1-compat tag-groups. Fix spec titles.

---
+++ api-docs/openapi/plugins/decorators/replace-shortcodes.js
@@ -0,0 +1,39 @@
+module.exports = ReplaceShortcodes;
+
+function replaceDocsUrl(node) {
+  const shortcode = /\{\{\% INFLUXDB_DOCS_URL \%\}\}/g;
+  const replacement = `/influxdb/${process.env.INFLUXDB_VERSION}`;
+  return node.description?.replace(shortcode, replacement);
+}
+
+/** @type {import('@redocly/openapi-cli').OasDecorator} */
+function docsUrl() {
+  return {
+    Info: {
+      leave(info, ctx) {
+        info.description = replaceDocsUrl(info);
+      }
+    },
+    PathItem: {
+      leave(pathItem, ctx) {
+        pathItem.description = replaceDocsUrl(pathItem);
+      }
+    },
+    Tag: {
+      leave(tag, ctx) {
+        tag.description = replaceDocsUrl(tag);
+      }
+    },
+    SecurityScheme: {
+      leave(scheme, ctx) {
+        scheme.description = replaceDocsUrl(scheme);
+      }
+    }
+  }
+}
+
+function ReplaceShortcodes() {
+  return {
+    docsUrl,
+  };
+}
