06b31f7e084dfb781f21388516d19872c0478b95
Scott Anderson <scott@influxdata.com>
2022-04-13 13:43:26 -0600

    WIP homepage style updates

--- assets/js/home-interactions.js
+++ assets/js/home-interactions.js
@@ -2,12 +2,14 @@ $('.api-btn-wrapper').click(function() {
   $('#api-btn p').fadeOut(100);
   setTimeout(function() {
     $('.api-links').fadeIn(200).addClass('open');
+    $('.api-btn-wrapper').addClass('open');
     $('#close-btn').fadeIn(200);
   }, 100);
 })
 
 $('#close-btn').click(function() {
   $('.api-links').fadeOut(100).removeClass('open');
+  $('.api-btn-wrapper').removeClass('open');
   $('#close-btn').fadeOut(100);
   setTimeout(function() {
     $('#api-btn p').fadeIn(200);
