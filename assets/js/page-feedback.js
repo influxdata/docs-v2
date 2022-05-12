cbd28cb93064432874dfe0d7d6aa0cbc3aa577e7
Scott Anderson <scott@influxdata.com>
2022-05-12 16:18:29 -0600

    finalize page feedback interactions

--- assets/js/page-feedback.js
+++ assets/js/page-feedback.js
@@ -1,47 +1,63 @@
+/*
+ * This file controls the interactions and life-cycles of the page feedback
+ * buttons and modal.
+ */
+
 // Collect data from the page path
-var pathArr = location.pathname.split('/').slice(1, -1)
-var pageData = {
+const pathArr = location.pathname.split('/').slice(1, -1)
+const pageData = {
+  host: location.hostname,
   path: location.pathname,
   product: pathArr[0],
-  version: (/^v\d/.test(pathArr[1]) || pathArr[1] === "cloud" ? pathArr[1].replace(/^v/, '') : "n/a")
+  version: (/^v\d/.test(pathArr[1]) || pathArr[1] === "cloud" ? pathArr[1].replace(/^v/, '') : "n/a"),
 }
 
-// Hijack form submission and send feedback data to be stored
-// Called by onSubmit for each feedback form
+// Hijack form submission and send feedback data to be stored.
+// Called by onSubmit in each feedback form.
 function submitFeedbackForm(formID) {
 
   // Collect form data, structure as an object, and remove fname honeypot
-  var formData = new FormData(document.forms[formID]);
-  var formDataObj = Object.fromEntries(formData.entries());
-  var {fname, ...feedbackData} = formDataObj;
+  const formData = new FormData(document.forms[formID]);
+  const formDataObj = Object.fromEntries(formData.entries());
+  const {fname, ...feedbackData} = formDataObj;
   
-  // Build line protocol fields form data
-  var fields = "";
+  // Build lp fields from form data
+  let fields = "";
   for (let key in feedbackData) {
     // Strip out newlines and escape double quotes if the field key is "feedback"
-    if (key == "feedback") {
-      fields += key + '="' + feedbackData[key].replace(/(\r\n|\n|\r)/gm, " ").replace(/(\")/gm, '\\"') + '",';
+    if (key == "feedback-text") {
+      fields += key + '="' + feedbackData[key].replace(/(\r\n|\n+|\r+)/gm, " ").replace(/(\")/gm, '\\"') + '",';
     } else {
       fields += key + "=" + feedbackData[key] + ",";
     }      
   }
   fields = fields.substring(0, fields.length -1);
 
-  // Build line protocol with page data and the fields string
-  var lp = `feedback,path=${pageData.path},product=${pageData.product},version=${pageData.version} ${fields}`
+  // Build lp using page data and the fields string
+  const lp = `feedback,host=${pageData.host},path=${pageData.path},product=${pageData.product},version=${pageData.version} ${fields}`
 
   // Use a honeypot form field to detect a bot
   // If the value of the honeypot field is greater than 0, the submitter is a bot
   function isBot() {
-    var honeypot = formData.get('fname');
+    const honeypot = formData.get('fname');
     return (honeypot.length > 0)
   }
   
-  // If the submitter is not a bot, do something
-  if (!isBot()) {console.log(lp);}
+  // If the submitter is not a bot, send the feedback data
+  if (!isBot()) {
+    xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://j32dswat7l.execute-api.us-east-1.amazonaws.com/prod');
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.setRequestHeader('Access-Control-Allow-Origin', `${location.protocol}//${location.host}`);
+    xhr.setRequestHeader('Content-Type', 'text/plain; charset=utf-8');
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.send(lp);
+  }
 
   return false;
 }
+
+/////////////////////////////// Click life-cycles //////////////////////////////
 
 // Trigger the lifecycle of page feedback (yes/no) radio select buttons
 function submitLifeCycle() {
@@ -61,6 +77,8 @@ function submitLifeCycleAndClose() {
   setTimeout(function() {toggleModal()}, 1800);
   return false;
 }
+
+//////////////////////////////// Event triggers ////////////////////////////////
 
 // Submit page feedback (yes/no) on radio select and trigger life cycle
 $('#pagefeedback input[type=radio]').change(function() {
