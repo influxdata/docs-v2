136f05ea70fd68a14e11afc717a7d153e8a6d19d
Jason Stirnaman <jstirnaman@influxdata.com>
2022-07-08 16:03:49 -0500

    chore: generate.sh generates all versions by default
    - Add a -c flag that only generates changed files (for development).
    - More refactoring.

--- api-docs/generate-api-docs.sh
+++ api-docs/generate-api-docs.sh
@@ -5,26 +5,27 @@ set -e
 function buildHugoTemplate {
   version=$1
   apiVersion=$2
-  module1=$3
+  outFilename=$3
 
-  V_LEN_INIT=$(wc -c $version$module1.tmp | awk '{print $1}')
-  cat "redoc-static${module1}.html" >> $version$module1.tmp
-  V_LEN=$(wc -c $version$module1.tmp | awk '{print $1}')
+  V_LEN_INIT=$(wc -c $version$outFilename.tmp | awk '{print $1}')
+  cat "redoc-static${outFilename}.html" >> $version$outFilename.tmp
+  V_LEN=$(wc -c $version$outFilename.tmp | awk '{print $1}')
 
   if ! [[ $V_LEN -gt $V_LEN_INIT  ]]
   then
-    echo "Error: bundle was not appended to $version$module1.tmp"
+    echo "Error: bundle was not appended to $version$outFilename.tmp"
     exit $?
   fi
 
-  rm -f "redoc-static${module1}.html"
+  rm -f "redoc-static${outFilename}.html"
   mkdir -p ../content/influxdb/$version/api
+  mv $version$outFilename.tmp ../content/influxdb/$version/api/$outFilename.html
 }
 
 function generateHtml {
   filePath=$1
-  titleVersion=$2
-  submodule=$3
+  outFilename=$2
+  titleVersion=$3
   titleSubmodule=$4
  
   echo "Bundling $filePath"
@@ -44,8 +45,41 @@ function generateHtml {
     --options.noAutoAuth \
     --templateOptions.version="$version" \
     --templateOptions.titleVersion="$titleVersion" \
-    --output="redoc-static$submodule.html"
+    --output="redoc-static$outFilename.html"
 }
+
+function showHelp {
+  echo "Usage: generate.sh <options>"
+  echo "Commands:"
+  echo "-c) Regenerate changed files. To save time in development, only regenerates files that differ from the master branch."
+  echo "-h) Show this help message."
+}
+
+# Get arguments
+
+generate_changed=1
+
+while getopts "hc" opt; do
+  case ${opt} in
+    h)
+      showHelp
+      exit 0
+      ;;
+    c)
+      generate_changed=0
+      ;;
+    \?)
+      echo "Invalid option: $OPTARG" 1>&2
+      showHelp
+      exit 22
+      ;;
+    :)
+      echo "Invalid option: $OPTARG requires an argument" 1>&2
+      showHelp
+      exit 22
+      ;;
+    esac
+done
 
 # Get list of versions from directory names
 versions="$(ls -d -- */ | grep -v 'node_modules' | grep -v 'openapi')"
@@ -55,7 +89,7 @@ do
   # Trim the trailing slash off the directory name
   version="${version%/}"
   menu="influxdb_$(echo $version | sed 's/\./_/g;s/v//g;')_ref"
-  if [ $version = "cloud" ]; then
+  if [[ $version == "cloud" ]]; then
     titleVersion="Cloud"
   else
     titleVersion="$version"
@@ -89,29 +123,41 @@ weight: 304
 
   # If the v2 spec file differs from master, regenerate the HTML.
   filePath="${version}/ref.yml"
-  fileChanged=$(git diff --name-status master -- ${filePath})
-  if [ ! -z "$fileChanged" ]; then
-    submodule=""
+  update=0
+  if [[ $generate_changed == 0 ]]; then
+    fileChanged=$(git diff --name-status master -- ${filePath})
+    if [[ -z "$fileChanged" ]]; then
+    update=1
+    fi
+  fi
+
+  if [[ $update -eq 0 ]]; then
+    outFilename="_index"
     titleSubmodule=""
-    generateHtml $filePath $titleVersion $submodule $titleSubmodule
+    generateHtml $filePath $outFilename $titleVersion $titleSubmodule
 
     # Create temp file with frontmatter and Redoc html
-    echo "$v2frontmatter" >> $version.tmp
-    buildHugoTemplate $version v2 $submodule && \
-    mv $version.tmp ../content/influxdb/$version/api/_index.html
+    echo "$v2frontmatter" >> $version$outFilename.tmp
+    buildHugoTemplate $version v2 $outFilename
   fi
 
   # If the v1 compatibility spec file differs from master, regenerate the HTML.
   filePath="${version}/swaggerV1Compat.yml"
-  fileChanged=$(git diff --name-status master -- ${filePath})
-  if [ ! -z "$fileChanged" ]; then
-    submodule="-v1-compat"
+  update=0
+  if [[ $generate_changed == 0 ]]; then
+    fileChanged=$(git diff --name-status master -- ${filePath})
+    if [[ -z "$fileChanged" ]]; then
+    update=1
+    fi
+  fi
+
+  if [[ $update -eq 0 ]]; then
+    outFilename="v1-compatibility"
     titleSubmodule="v1 compatibility"
-    generateHtml $filePath $titleVersion $submodule $titleSubmodule
+    generateHtml $filePath $outFilename $titleVersion $titleSubmodule
 
     # Create temp file with frontmatter and Redoc html
-    echo "$v1compatfrontmatter" >> $version$submodule.tmp
-    buildHugoTemplate $version v1 $submodule && \
-    mv $version$submodule.tmp ../content/influxdb/$version/api/v1-compatibility.html
+    echo "$v1compatfrontmatter" >> $version$outFilename.tmp
+    buildHugoTemplate $version v1 $outFilename
   fi
 done
