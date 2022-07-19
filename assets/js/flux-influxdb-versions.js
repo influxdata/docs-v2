5ce0d1ec47111d01473a3f32564a47b23f379234
Scott Anderson <sanderson@users.noreply.github.com>
2022-07-19 15:14:10 -0600

    Show what versions of InfluxDB Flux functions are supported in (#4219)
    * automatically build flux version data file
    
    * rendered modals on each stdlib page
    
    * polish on flux-influxdb version support
    
    * reverted modified frontmatter
    
    * added new page that shows what flux versions are packaged with each InfluxDB version
    
    * added comment to js file
    
    * updated to address PR feedback
    
    * moved flux build scripts into their own directory
    
    * updated inject-flux-frontmatter script to work in subdirectory
    
    * updated flux-versions script to work in nested directory
    
    * fixed bug in flux-versions script

---
+++ assets/js/flux-influxdb-versions.js
@@ -0,0 +1,25 @@
+/*
+Interactions related to the Flux/InfluxDB version modal
+*/
+
+const fluxInfluxDBModal = '.modal-content#flux-influxdb-versions'
+
+// Check for deprecated or pending versions
+function keysPresent() {
+  var list = $(fluxInfluxDBModal + ' .version-list')
+
+  return {
+    pending: list.find('.pending').length !== 0,
+    deprecated: list.find('.deprecated').length !== 0,
+  }
+}
+
+// Only execute if the Flux/InfluxDB modal is present in the DOM
+if ($(fluxInfluxDBModal).length > 0) {
+  var presentKeys = keysPresent()
+
+  // Remove color key list items if the colors/states are present in the version list
+  if (presentKeys.pending === false) { $(fluxInfluxDBModal + ' .color-key #pending-key' ).remove() }
+  if (presentKeys.deprecated === false) { $(fluxInfluxDBModal + ' .color-key #deprecated-key' ).remove() }
+  if (presentKeys.pending === false && presentKeys.deprecated === false) { $(fluxInfluxDBModal + ' .color-key' ).remove() }
+}
--- content/flux/v0.x/stdlib/testing/benchmark.md
+++ content/flux/v0.x/stdlib/testing/benchmark.md
@@ -23,7 +23,7 @@ documentation is generated.
 To make updates to this documentation, update the function comments above the
 function definition in the Flux source code:
 
-https://github.com/influxdata/flux/blob/master/stdlib/testing/testing.flux#L372-L376
+https://github.com/influxdata/flux/blob/master/stdlib/testing/testing.flux#L353-L357
 
 Contributing to Flux: https://github.com/influxdata/flux#contributing
 Fluxdoc syntax: https://github.com/influxdata/flux/blob/master/docs/fluxdoc.md
