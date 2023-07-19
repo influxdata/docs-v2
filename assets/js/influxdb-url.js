76cf977706ba9287ed2e10b9a0970979f23a3e51
Scott Anderson <sanderson@users.noreply.github.com>
2023-07-19 14:26:00 -0600

    Add Chronograf documentation to 3.0 (#5039)
    * add chronograf documentation to 3.0, clean up cloud URL modal, closes #5037
    
    * add more info about fully-qualified measurements in chronograf doc
    
    * Apply suggestions from code review
    
    Co-authored-by: Jason Stirnaman <stirnamanj@gmail.com>
    
    ---------
    
    Co-authored-by: Jason Stirnaman <stirnamanj@gmail.com>

--- assets/js/influxdb-url.js
+++ assets/js/influxdb-url.js
@@ -18,6 +18,8 @@ function context() {
     return "cloud"
   } else if (/\/influxdb\/cloud-dedicated/.test(window.location.pathname)) {
     return "dedicated"
+  } else if (/\/influxdb\/cloud-serverless/.test(window.location.pathname)) {
+    return "serverless"
   } else if (/\/(enterprise_|influxdb).*\/v[1-2]\.[0-9]{1,2}\//.test(window.location.pathname)) {
     return "oss/enterprise"
   } else {
@@ -250,7 +252,7 @@ function appendUrlSelector() {
 
   var appendToUrls = [ placeholderUrls.cloud, placeholderUrls.oss, placeholderUrls.dedicated ]
 
-  if (context() === "cloud") {
+  if (context() === "cloud" || "serverless") {
     var selectorText = "InfluxDB Cloud Region"
   } else if (context() === "dedicated") {
     var selectorText = "Set dedicated cluster URL"
