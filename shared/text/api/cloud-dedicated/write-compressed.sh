c08cded01f757d9c4d56c77e3c79981c89f276b7
Jason Stirnaman <stirnamanj@gmail.com>
2023-09-15 18:07:58 -0500

    fix: v3 compressed examples, gzip capitalization

--- shared/text/api/cloud-dedicated/write-compressed.sh
+++ shared/text/api/cloud-dedicated/write-compressed.sh
@@ -1,12 +1,10 @@
-curl --request POST "https://cluster-id.influxdb.io/api/v2/write" \
-  --header "Authorization: Token DATABASE_TOKEN" \
-  --header "Content-Encoding: gzip" \
-  --data-urlencode "org=ignored" \
-  --data-urlencode "bucket=DATABASE_NAME" \
-  --data-urlencode "precision=s" \
-  --data-raw "
-mem,host=host1 used_percent=23.43234543 1641024000
+echo "mem,host=host1 used_percent=23.43234543 1641024000
 mem,host=host2 used_percent=26.81522361 1641027600
 mem,host=host1 used_percent=22.52984738 1641031200
-mem,host=host2 used_percent=27.18294630 1641034800
-"
+mem,host=host2 used_percent=27.18294630 1641034800" | gzip > system.gzip \
+
+curl --request POST "https://cluster-id.influxdb.io/api/v2/write" \
+  --header "Authorization: Token API_TOKEN" \
+  --header "Content-Type: text/plain; charset=utf-8" \
+  --header "Content-Encoding: gzip" \
+  --data-binary @system.gzip
