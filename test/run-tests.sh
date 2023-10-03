35f3f09edbb975d6f62d26b6b15248e6e0f4df82
Jason Stirnaman <stirnamanj@gmail.com>
2023-10-03 10:09:47 -0500

    Python test placeholders (#5161)
    * chore(ci): Update vocab.
    
    * chore(test): Set language-specific replacements for code sample tests
    
    This is a bit of kludge to allow for language-specific replacement patterns
    while keeping the code samples valid when a user replaces placeholder strings with their own and copies the sample.
    This is necessary for executing (not for validating syntax) of the code samples.
    
    - Workaround for identifying language-specific replacements for pytest-codeblocks test runs.
    - Uses Python f-strings to identify Python-specific placeholders.
    - To import os for your code sample and hide it from users, wrap it in a comment and use the `pytest-codeblocks:cont` comment tag. See https://github.com/nschloe/pytest-codeblocks/blob/d0ca7e22b8e4d0c2d8807a9cf9e1f8ca7ef20442/README.md?plain=1#L117

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
-for file in "$TEST_DIR"/*; do
+for file in `find . -type f` ; do
   if [ -f "$file" ]; then
+    echo "PROCESSING $file"
+
     # Replace placeholder values with environment variables.
-    sed -i '' 's|https:\/\/{{< influxdb/host >}}|$INFLUX_HOST|g;
-    s/API_TOKEN/$INFLUX_TOKEN/g;
+    # Non-language-specific replacements.
+    sed -i 's|https:\/\/{{< influxdb/host >}}|$INFLUX_HOST|g;
+    ' $file
+
+    # Python-specific replacements.
+    # Use f-strings to identify placeholders in Python while also keeping valid syntax if
+    # the user replaces the value.
+    # Remember to import os for your example code.
+    sed -i 's/f"DATABASE_TOKEN"/os.getenv("INFLUX_TOKEN")/g;
+    s/f"API_TOKEN"/os.getenv("INFLUX_TOKEN")/g;
+    s/f"BUCKET_NAME"/os.getenv("INFLUX_DATABASE")/g;
+    s/f"DATABASE_NAME"/os.getenv("INFLUX_DATABASE")/g;
+    s|f"{{< influxdb/host >}}"|os.getenv("INFLUX_HOSTNAME")|g;
+    ' $file
+
+    # Shell-specific replacements.
+    sed -i 's/API_TOKEN/$INFLUX_TOKEN/g;
     s/DATABASE_TOKEN/$INFLUX_TOKEN/g;
     s/BUCKET_NAME/$INFLUX_DATABASE/g;
     s/DATABASE_NAME/$INFLUX_DATABASE/g;' \
     $file
   fi
+  cat $file
 done
 
-pytest --codeblocks $TEST_DIR
+pytest --codeblocks .
