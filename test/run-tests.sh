f7ce583505d77816542a216c57592c6b46eceac7
Jason Stirnaman <stirnamanj@gmail.com>
2023-11-22 16:54:23 -0600

    chore(ci): Tests: substitute placeholders in JSON body parameters

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -49,9 +49,13 @@ for file in `find . -type f` ; do
     ' $file
 
     # Shell-specific replacements.
+    ## In JSON Heredoc
+    sed -i 's|"orgID": "ORG_ID"|"orgID": "$INFLUX_ORG"|g;
+    s|"name": "BUCKET_NAME"|"name": "$INFLUX_DATABASE"|g;' \
+    $file
+
     sed -i 's/API_TOKEN/$INFLUX_TOKEN/g;
     s/ORG_ID/$INFLUX_ORG/g;
-    s/ORG/$INFLUX_ORG/g;
     s/DATABASE_TOKEN/$INFLUX_TOKEN/g;
     s/BUCKET_NAME/$INFLUX_DATABASE/g;
     s/DATABASE_NAME/$INFLUX_DATABASE/g;
@@ -89,8 +93,12 @@ gpg -q --batch --yes --delete-key D8FF8E1F7DF8B07E > /dev/null 2>&1
 
 # Run test commands with options provided in the CMD of the Dockerfile.
 # pytest rootdir is the directory where pytest.ini is located (/test).
+if [ -d ./content/influxdb/cloud-dedicated/ ]; then
 echo "Running cloud-dedicated tests..."
 pytest --codeblocks --envfile $BASE_DIR/.env.dedicated ./content/influxdb/cloud-dedicated/ $@
+fi
 
+if [ -d ./content/influxdb/cloud-serverless/ ]; then
 echo "Running cloud-serverless tests..."
 pytest --codeblocks --envfile $BASE_DIR/.env.serverless ./content/influxdb/cloud-serverless/ $@
+fi
