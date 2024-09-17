0091f3b024084105da88064f1f707213ee7a45f8
Jason Stirnaman <stirnamanj@gmail.com>
2024-09-17 09:06:11 -0500

    WIP: draft TODO. Simplify code.

--- api-docs/scripts/generate-openapi-articles.js
+++ api-docs/scripts/generate-openapi-articles.js
@@ -18,45 +18,42 @@ const execCommand = (command) => {
 };
 
 // Function to generate data from OpenAPI
-function generateDataFromOpenAPI(specFile, dataOutPath, articleOutPath) {
-  if (!fs.existsSync(dataOutPath)) {
-    fs.mkdirSync(dataOutPath, { recursive: true });
+function generateApiData(specFile, dataPath) {
+  if (!fs.existsSync(dataPath)) {
+    fs.mkdirSync(dataPath, { recursive: true });
   }
 
   openapiDocs.openapiToData({
-    dataOutPath,
-    articleOutPath,
-    specFile
+    specFile,
+    dataPath
   });
 };
 
 // Example usage of generateDataFromOpenAPI function
 // generateDataFromOpenAPI('path/to/openapi-file.yml', 'path/to/paths-output-folder', 'path/to/metadata-output-folder');
 
-const generatePagesFromArticleData = async (articlesPath, contentPath) => {
+const generateApiPages = async (dataPath, contentPath) => {
   let config = {
     root: '.', //Root hugo folder, can be empty
-    dataFolder: articlesPath, //Data folder path (will fetch ALL files from here)
+    dataFolder: dataPath, //Data folder path (will fetch ALL files from here)
     type: "api", //Type name [basically layout] (save it under "layouts/NAME/single.html" or themes/THEME/layouts/NAME/single.html). Can be overridden on individual pages by defining "type" under "fields"
-    pages: "articles", //Pages element in your data, in case it's "posts" or "articles" etc.
+    pages: "paths", //Pages element in your data, in case it's "posts" or "articles" etc.
     contentPath: contentPath, //Path to content directory (in case it's not "content")
     hugoPath: `${DOCS_ROOT}/node_modules/.bin/hugo-extended` //Path to hugo binary (if global, e.g. /snap/bin/hugo)
   }
   configJson = `'${JSON.stringify(config)}'`;
-  console.log('Clean...')
-  execCommand(
-    `node ./node_modules/hugo-data-to-pages/hugo.js clean --force --config=${configJson}`
-  )
+  // console.log('Clean...')
+  // execCommand(
+  //   `node ./node_modules/hugo-data-to-pages/hugo.js clean --force --config=${configJson}`
+  // )
   console.log('Generate...')
   execCommand(
     `node ./node_modules/hugo-data-to-pages/hugo.js generate --config=${configJson}`
   )
 }
 
-// To output to a public folder where JavaScript can access it, use /static/<namespace>--for example: /static/openapi/
 const config = {
-  dataOutPath: path.join(DOCS_ROOT, '/data/api/influxdb'),
-  metadataOutPath: path.join(DOCS_ROOT, `/data/api-metadata/influxdb`),
+  dataDir: path.join(DOCS_ROOT, '/data/api/influxdb'),
   apis: [
     {
       name: 'cloud-v2',
@@ -74,12 +71,19 @@ const config = {
 config.apis.forEach(api => {
   // Execute the getswagger.sh script
   execCommand(`${path.join(DOCS_ROOT, '/api-docs/getswagger.sh')} ${api.name} -B`);
-  const dataOut = path.join(config.dataOutPath, api.name);
-  const metadataOut = path.join(config.metadataOutPath, api.name);
-  if (!fs.existsSync(dataOut)) {
-    fs.mkdirSync(dataOut, { recursive: true });
+  // Copy the generated spec to /data
+  const specDataDir = path.join(config.dataDir, api.name);
+  if (!fs.existsSync(specDataDir)) {
+    fs.mkdirSync(specDataDir, { recursive: true });
   }
-  fs.copyFileSync(api.spec_file, path.join(dataOut, api.name + '.yml'));
-  generateDataFromOpenAPI(api.spec_file, dataOut, metadataOut);
-  generatePagesFromArticleData(metadataOut, api.pages_dir);
+  const specDataFile = path.join(specDataDir, api.name + '.yml');
+  fs.copyFileSync(api.spec_file, specDataFile);
+  const apiPathsDir = path.join(specDataDir, '/paths');
+  generateApiData(api.spec_file, apiPathsDir);
+  
+  // Remove old pages
+  fs.rmSync(api.pages_dir, {recursive: true, force: true});
+  // generateDataFromOpenAPI(api.spec_file, dataOut, metadataOut);
+  fs.mkdirSync(api.pages_dir, { recursive: true });
+  generateApiPages(apiPathsDir, api.pages_dir);
 });
