947554dba9a4b3e7d3cc32335e5f96edb92519d8
Jason Stirnaman <stirnamanj@gmail.com>
2025-05-12 17:05:36 -0500

    WIP: removed remaining scripts from footer/javascript.html. Migrated to components and utilities. Conditionally imports Mermaid if page has a diagram.

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,7 @@ import $ from 'jquery';
 import * as apiLibs from './api-libs.js';
 import * as codeControls from './code-controls.js';
 import * as contentInteractions from './content-interactions.js';
+import * as datetime from './datetime.js';
 import { delay } from './helpers.js';
 import { InfluxDBUrl } from './influxdb-url.js';
 import * as localStorage from './local-storage.js';
@@ -32,11 +33,14 @@ import * as v3Wayfinding from './v3-wayfinding.js';
 import AskAITrigger from './ask-ai-trigger.js';
 import CodePlaceholder from './code-placeholders.js';
 import { CustomTimeTrigger } from './custom-timestamps.js';
+import Diagram from './components/diagram.js';
 import FeatureCallout from './feature-callouts.js';
+import FluxGroupKeysDemo from './flux-group-keys.js';
 import FluxInfluxDBVersionsTrigger from './flux-influxdb-versions.js';
 import KeyBinding from './keybindings.js';
 import ListFilters from './list-filters.js';
 import ProductSelector from './version-selector.js';
+import ReleaseToc from './release-toc.js';
 import { SearchButton } from './search-button.js';
 import SidebarSearch from './components/sidebar-search.js';
 import { SidebarToggle } from './sidebar-toggle.js';
@@ -57,11 +61,14 @@ const componentRegistry = {
   'ask-ai-trigger': AskAITrigger,
   'code-placeholder': CodePlaceholder,
   'custom-time-trigger': CustomTimeTrigger,
+  'diagram': Diagram,
   'feature-callout': FeatureCallout,
+  'flux-group-keys-demo': FluxGroupKeysDemo,
   'flux-influxdb-versions-trigger': FluxInfluxDBVersionsTrigger,
   'keybinding': KeyBinding,
   'list-filters': ListFilters,
   'product-selector': ProductSelector,
+  'release-toc': ReleaseToc,
   'search-button': SearchButton,
   'sidebar-search': SidebarSearch,
   'sidebar-toggle': SidebarToggle,
@@ -140,6 +147,7 @@ function initModules() {
   apiLibs.initialize();
   codeControls.initialize();
   contentInteractions.initialize();
+  datetime.initialize();
   InfluxDBUrl();
   notifications.initialize();
   pageFeedback.initialize();
