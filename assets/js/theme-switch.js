c43df7508feed62fb477f063964ae8312dadd813
Jason Stirnaman <stirnamanj@gmail.com>
2025-06-02 15:14:23 -0500

    chore(js): Remove unnecessary JS module config, remove redundant index.js

--- assets/js/theme-switch.js
+++ assets/js/theme-switch.js
@@ -1,20 +1,21 @@
 import Theme from './theme.js';
 
 export default function ThemeSwitch({ component }) {
-  if ( component == undefined) {
+  if (component === undefined) {
     component = document;
   }
-  component.querySelectorAll(`.theme-switch-light`).forEach((button) => {
-    button.addEventListener('click', function(event) {
+
+  component.querySelectorAll('.theme-switch-light').forEach((button) => {
+    button.addEventListener('click', function (event) {
       event.preventDefault();
-      Theme({component, style: 'light-theme' });
+      Theme({ component, style: 'light-theme' });
     });
   });
 
-  component.querySelectorAll(`.theme-switch-dark`).forEach((button) => {
-    button.addEventListener('click', function(event) {
+  component.querySelectorAll('.theme-switch-dark').forEach((button) => {
+    button.addEventListener('click', function (event) {
       event.preventDefault();
-      Theme({component, style: 'dark-theme' });
+      Theme({ component, style: 'dark-theme' });
     });
   });
 }
