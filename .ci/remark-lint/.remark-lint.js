efd288fdb8d5cf71d261dd5519a88eb7400cad88
Jason Stirnaman <jstirnaman@influxdata.com>
2025-10-31 15:07:01 -0400

    fix(lint): disable remark formatting for content files to preserve GitHub Alerts (#6502)
    Remark-stringify escapes square brackets in GitHub Alerts syntax
    (> [!Note] becomes > \[!Note]), breaking alert rendering.
    
    Changes:
    - Remove lint-markdown-content pre-commit hook from lefthook.yml
    - Configure remark-lint-no-undefined-references to allow GitHub Alerts
    - Add remark-lint-no-undefined-references dependency
    - Unescaped GitHub Alerts in content/create.md
    
    Content files now preserve GitHub Alerts syntax while instruction files
    continue to use remark auto-formatting (they don't contain alerts).
    Vale provides adequate style linting for content files.
    
    Closes #6501
    See: https://github.com/remarkjs/remark-gfm/issues/53

--- .ci/remark-lint/.remark-lint.js
+++ .ci/remark-lint/.remark-lint.js
@@ -4,22 +4,31 @@ import remarkPresetLintMarkdownStyleGuide from 'remark-preset-lint-markdown-styl
 import remarkFrontmatter from 'remark-frontmatter';
 import remarkFrontmatterSchema from 'remark-lint-frontmatter-schema';
 import remarkNoShellDollars from 'remark-lint-no-shell-dollars';
+import remarkLintNoUndefinedReferences from 'remark-lint-no-undefined-references';
 import remarkToc from 'remark-toc';
 
 const remarkConfig = {
   settings: {
     bullet: '-',
-    plugins: [
-      remarkPresetLintConsistent,
-      remarkPresetLintRecommended,
-      remarkPresetLintMarkdownStyleGuide,
-      remarkFrontmatter,
-      remarkFrontmatterSchema,
-      remarkNoShellDollars,
-      // Generate a table of contents in `## Contents`
-      [remarkToc, { heading: '' }],
-    ],
   },
+  plugins: [
+    remarkPresetLintConsistent,
+    remarkPresetLintRecommended,
+    remarkPresetLintMarkdownStyleGuide,
+    remarkFrontmatter,
+    remarkFrontmatterSchema,
+    remarkNoShellDollars,
+    // Override no-undefined-references to allow GitHub Alerts syntax
+    // This prevents lint warnings for [!Note], [!Tip], etc. in blockquotes
+    [
+      remarkLintNoUndefinedReferences,
+      {
+        allow: ['!Note', '!Tip', '!Important', '!Warning', '!Caution'],
+      },
+    ],
+    // Generate a table of contents in `## Contents`
+    [remarkToc, { heading: '' }],
+  ],
 };
 
 export default remarkConfig;
