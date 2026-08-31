60b10185af0ae8ba5d912ebc0c5891dade70205d
jstirnaman <212227+jstirnaman@users.noreply.github.com>
2026-08-31 09:13:19 +0000

    chore(deps): update Vale to v3.19.0

--- .ci/vale/vale.sh
+++ .ci/vale/vale.sh
@@ -21,7 +21,7 @@ set -euo pipefail
 #       --minAlertLevel=suggestion \
 #       --config=content/influxdb/cloud-dedicated/.vale.ini
 
-VALE_VERSION="3.18.0"
+VALE_VERSION="3.19.0"
 VALE_MAJOR_MIN=3
 
 if command -v vale &>/dev/null; then
